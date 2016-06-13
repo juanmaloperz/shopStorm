638c9ecf66cbccc0e4f0b372a3023d191c362dd0
Juan Lopez <juanmaloperz@gmail.com>
2016-06-13 17:38:31 -0500

    add coppy

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -2,7 +2,10 @@ var React = require('react');
 
 var Example = (props) =>{
   return(
+    <div>
     <h2> Example!</h2>
+    <p> Bienvenidos al ejmeplo!</p>
+    </div>
   )
 };
 module.exports = Example;
