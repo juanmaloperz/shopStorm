f9b15ba51f2157d5615c6a3f2e0ece80e0532f63
Juan Lopez <juanmaloperz@gmail.com>
2016-06-14 10:59:28 -0500

    Change about status

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -2,7 +2,10 @@ var React = require('react');
 
    var About = (props) => {
      return(
+       <div>
        <h2>About</h2>
+       <p> Bienvenido a la pagina de About</p>
+       </div>
      )
    };
 
