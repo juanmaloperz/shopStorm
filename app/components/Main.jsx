e4a5f30e03e878bab7193112ef952b277c909349
Juan Lopez <juanmaloperz@gmail.com>
2016-06-14 18:05:30 -0500

    deleting main c

--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -7,7 +7,6 @@ var Main = (props) =>{
   <Nav/>
   <div className="row">
       <div className="medium-6 large-4 columns small-centered">
-    <h2> Main Component</h2>
     {props.children}
       </div>
     </div>
