17b57df1160470afa5c803ce975ee410085a0884
Juan Lopez <juanmaloperz@gmail.com>
2016-06-14 16:52:27 -0500

    styling example and Main

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,10 +1,18 @@
 var React = require('react');
-
+var {Link} = require('react-router');
 var Example = (props) =>{
   return(
     <div>
-    <h2> Example!</h2>
-    <p> Bienvenidos al ejmeplo!</p>
+    <h1 className="text-center"> Example!</h1>
+    <p> Here are a few example locations to try out</p>
+    <ol>
+       <li>
+        <Link to='/?location=Cucuta'> Cucuta,Colombia</Link>
+       </li>
+       <li>
+          <Link to='/?location=Bogota'>Bogota, Colombia</Link>
+       </li>
+    </ol>
     </div>
   )
 };
--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -5,8 +5,12 @@ var Main = (props) =>{
   return(
 <div>
   <Nav/>
+  <div className="row">
+      <div className="medium-6 large-4 columns small-centered">
     <h2> Main Component</h2>
     {props.children}
+      </div>
+    </div>
     </div>
   )
 }
