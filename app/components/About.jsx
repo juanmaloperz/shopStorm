83733975feaaee9f891c91a9910a12f678d3eaf8
Juan Lopez <juanmaloperz@gmail.com>
2016-06-14 17:53:28 -0500

    style about

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,10 +1,12 @@
 var React = require('react');
+var {Link} = require('react-router');
 
    var About = (props) => {
      return(
        <div>
-       <h2>About</h2>
-       <p> Bienvenido a la pagina de About</p>
+       <h1 className="text-center">About</h1>
+       <p> This webapp was made by Juan Manuel López. I made it in  <a href="https://facebook.github.io/react/">  React </a></p>
+        <p>  I took the weather information from <a href="http://openweathermap.org"> Open Weather Map API</a></p>
        </div>
      )
    };
