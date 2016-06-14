b236f8abf4c71a0b893ddd785bc9184e5599010f
Juan Lopez <juanmaloperz@gmail.com>
2016-06-14 14:31:11 -0500

    Style nav bar with Foundation

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -2,14 +2,41 @@ var React = require('react');
 var {Link} = require('react-router');
 
 var Nav = React.createClass({
+  onSearch: function(e){
+    e.preventDefault();
+    alert("its not wired up");
+  },
   render: function(){
     return(
-    <div>
-    <h2>  This is the nav component</h2>
-   <Link to="/"> Get Weather</Link>
-   <Link to="/about"> About</Link>
-   <Link to="/examples"> Example</Link>
-   </div>
+ <div className="top-bar">
+  <div className="top-bar-left">
+    <ul className="menu">
+     <li className="menu-text"> The Weather App </li>
+      <li>
+        <Link to="/"> Get Weather</Link>
+      </li>
+      <li>
+        <Link to="/about"> About</Link>
+      </li>
+      <li>
+        <Link to="/examples"> Example</Link>
+      </li>
+    </ul>
+</div>
+  <div className="top-bar-rigth">
+    <form onsubmit={this.onSearch}>
+     <ul className="menu">
+       <li>
+          <input type="search" placeholder="Write the city"/>
+        </li>
+       <li>
+          <input type="button" value=" Get Weather!"/>
+       </li>
+     </ul>
+    </form>
+  </div>
+ </div>
+
     );
   }
   });
