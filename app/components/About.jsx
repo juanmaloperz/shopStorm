5ae847e5f8d2634afda732ce5d9771fbe0e10119
Juan Lopez <juanmaloperz@gmail.com>
2016-06-17 11:44:02 -0500

    Styling whit css

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -4,7 +4,7 @@ var {Link} = require('react-router');
    var About = (props) => {
      return(
        <div>
-       <h1 className="text-center">About</h1>
+       <h1 className="text-center page-title">About</h1>
        <p> This webapp was made by Juan Manuel López. I made it in  <a href="https://facebook.github.io/react/">  React </a></p>
         <p>  I took the weather information from <a href="http://openweathermap.org"> Open Weather Map API</a></p>
        </div>
